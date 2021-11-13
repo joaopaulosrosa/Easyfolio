33f8ac8ce2046c0e9439c28222f5c90309d9ada1
Gustavo Meneses <gdameneses@gmail.com>
2021-11-13 18:38:46 +0000

    added devise views, implemented method that creates watchlist with user and assigns to user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  resources :watchlists, only: [:show, :create]
 end
