87be1016c66f47c5ad505da0558d47d2fc2947c1
Joao Paulo Rosa <joaopaulosrosa@gmail.com>
2021-11-18 15:09:53 -0300

    now watchlist is fetching CoinGecko API with more informations

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -3,11 +3,17 @@ class WatchlistsController < ApplicationController
   def show
     @watchlist = Watchlist.where(user: current_user)[0]
     @tickers = generate_tickers
-    @cryptowatch = CoinTools::Cryptowatch.new
+    @data = call_coin_gecko
     authorize @watchlist
   end
 
   def generate_tickers
-    @watchlist.coin_bookmarks.pluck(:coin_ticker).map{|coin| "#{coin}usd" }
+    @watchlist.coin_bookmarks.pluck(:coin_ticker).map{|coin| "#{coin}" }
+  end
+
+  def call_coin_gecko
+    url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
+    data = HTTParty.get(url).body
+    JSON.parse(data)
   end
 end
