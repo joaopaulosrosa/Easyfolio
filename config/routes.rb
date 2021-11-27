25349df1e55af3015a1b44718547f597a0fe8726
Elias Chawki <chawki.elias@gmail.com>
2021-11-27 09:16:56 -0500

    landing page updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'about', to: 'pages#about'
   get 'coin_explore', to: 'pages#explore'
-
+  get 'dashboard', to: 'pages#dashboard'
   resources :users do
     resources :wallets, only: [ :new, :create, :destroy, :show, :index ] do
       resources :assets
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   resources :watchlists, only: [:show, :create] do
     resources :coin_bookmarks, only: [:new, :create, :destroy]
   end
+
 end
