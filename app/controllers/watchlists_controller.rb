fb4f74ac37b7a1846deb45fde93d04fa43efb4ce
Barbara Santos <barbaransantos1@gmail.com>
2021-12-04 16:48:15 -0300

    comented many code in watchlist

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -7,6 +7,41 @@ class WatchlistsController < ApplicationController
     authorize @watchlist
   end
 
+  def explore
+    @top10 = call_coin_gecko[0..9]
+    url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
+    if params[:query].present?
+      data = HTTParty.get(url).body
+      data_json = JSON.parse(data)
+      if data_json.select{ |key| key["symbol"] == params[:query] }.empty?
+        flash[:alert] = "This coin doesn't exist, type again."
+      else
+        response = data_json.select{ |key| key["symbol"] == params[:query] }.first
+        @coin = response.transform_keys(&:to_sym)
+        @btc = get_btc
+      end
+    end
+  end
+
+  def coin
+    
+    url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
+    if params[:query].present?
+      data = HTTParty.get(url).body
+      data_json = JSON.parse(data)
+      query = params[:query].present? ? params[:query].downcase : params[:query]
+      id_query = data_json.select{ |key| key["symbol"].downcase == query || key["name"].downcase == query}
+      if id_query.empty? || query.blank?
+        redirect_to coin_explore_path
+        flash[:alert] = "This coin doesn't exist, type again."
+      else
+        @coin = get_coin(id_query)
+      end
+    else
+      redirect_to coin_explore_path
+    end
+
+  end
 
   private
 
