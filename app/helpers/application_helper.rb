dfbbe2ab99955e13e623c5ac634dc58d963f4097
Joao Paulo Rosa <joaopaulosrosa@gmail.com>
2021-12-22 00:20:00 -0300

    now asset can be either downcase or upcase that will not break

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def coin(data, asset)
-    data.select{ |key| key["symbol"] == asset.coin_ticker }.first.transform_keys(&:to_sym)
+    data.select{ |key| key["symbol"] == asset.coin_ticker.downcase }.first.transform_keys(&:to_sym)
   end
 
   def market_cap
